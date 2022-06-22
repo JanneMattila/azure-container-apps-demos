1c6b32817f73706b4ac52658cb99fb7ea5b816ab
Janne Mattila <janne.mattila@hotmail.fi>
2022-06-22 08:12:11 +0300

    Fix script

--- azureautomationapp/run.sh
+++ azureautomationapp/run.sh
@@ -11,6 +11,6 @@ echo "Running az cli based automation..."
 # Use manual method:
 # Example from
 # https://github.com/JanneMattila/hassio-addon-azuredns/blob/main/addon/run.sh
-ACCESS_TOKEN=$(curl --no-progress-meter --silent "$IDENTITY_ENDPOINT?api-version=2019-08-01&client_id=$AZURE_CLIENT_ID&resource=https://management.azure.com/" -H "X-IDENTITY-HEADER: $IDENTITY_HEADER")
+ACCESS_TOKEN=$(curl --no-progress-meter --silent "$IDENTITY_ENDPOINT?api-version=2019-08-01&client_id=$AZURE_CLIENT_ID&resource=https://management.azure.com/" -H "X-IDENTITY-HEADER: $IDENTITY_HEADER" | jq -r .access_token)
 SUBSCRIPTIONS=$(curl --no-progress-meter --silent -H "Authorization: Bearer $ACCESS_TOKEN" "https://management.azure.com/subscriptions?api-version=2018-05-01")
 echo $SUBSCRIPTIONS
